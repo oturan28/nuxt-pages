2c222d56c2993d84d0bd0731b6bb600353c5518a
Osman Turan <turan@anwalt.de>
2024-01-18 11:15:27 +0100

    asdsad

--- pages/gittest/index.vue
+++ pages/gittest/index.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
     <h1>git test</h1>
-    <h1>git test</h1>
-    <h1>git test</h1>
-    <h1>git test</h1>
-    <h1>git test</h1>
-    <h1>git test</h1>
-    <h1>git test</h1>
-    <h1>git test</h1>
   </div>
 </template>
