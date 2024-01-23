ba6b5e13e08f71c64152537ba6a5448aa3d4f981
Osman Turan <turan@anwalt.de>
2024-01-23 13:53:51 +0100

    asdsad

---
+++ components/Quote.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>This is my quote: <q>Build a future where people live in harmony with nature.</q></p>
+  <p>This is my quote: <q>Build a future where people live in harmony with nature.</q></p>
+  <p>This is my quote: <q>Build a future where people live in harmony with nature.</q></p>
+  <p>This is my quote: <q>Build a future where people live in harmony with nature.</q></p>
+</template>
