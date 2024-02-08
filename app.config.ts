5725cc9e035806d6959446e374d1e81fed3ce528
Osman Turan <turan@anwalt.de>
2024-02-08 08:39:19 +0100

    asdsad

---
+++ app.config.ts
@@ -0,0 +1,7 @@
+export default defineAppConfig({
+    board: [
+        { image: 'https://placehold.co/40',name: 'John Doe 545454', title: 'CEO', linkedInUrl: 'https://www.linkedin.com/' },
+        { image: 'https://placehold.co/40', name: 'John Doe', title: 'CEO', linkedInUrl: 'https://www.linkedin.com/' },
+        { image: 'https://placehold.co/40', name: 'John Doe', title: 'CEO', linkedInUrl: 'https://www.linkedin.com/' },
+    ]
+})
