f2ed1dd865a24e2012bbb889f324d25a79c1d3e4
Osman Turan <turan@anwalt.de>
2024-07-07 21:45:07 +0200

    island test

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { AnwGridFooter } from '@anwalt.de/tailwind-design-system';
 </script>
 <template>
   <div>
-    <AnwGridFooter />
+    <p>footer</p>
   </div>
 </template>
 <style lang="css">
-@import '@/node_modules/@anwalt.de/tailwind-design-system/src/index.css';
-@import '@/node_modules/@anwalt.de/tailwind-design-system/dist/style.css';
+
 </style>
